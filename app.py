00174b3de2f5377f0d7428267df071c80b2e52e0
Muchelikeertan <keertanmucheli@gmail.com>
2026-09-21 18:33:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+import pandas as pd
+
+app=FastAPI(title="Air Quality Detection")
+
+model=joblib.load("air_quality_model.pkl")
+
+class AirQualityInput(BaseModel):
+    pm2_5: float
+    pm10: float
+    carbon_monoxide: float
+    nitrogen_dioxide: float
+    sulphur_dioxide: float
+    ozone: float
+
+
+@app.get("/")
+def home():
+    return{
+        "message":"Welcome to Air Quality Detection"
+    }
+
+@app.post("/predict")
+def predict(data : AirQualityInput):
+  prediction=model.predict([[
+      data.pm2_5,
+      data.pm10,
+      data.carbon_monoxide,
+      data.nitrogen_dioxide,
+      data.sulphur_dioxide,
+      data.ozone
+  ]])
+  return{
+      "prediction":prediction[0]
+  }
